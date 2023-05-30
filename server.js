ec4e1e04558991ce9b9184ebbbfec7470375fb0c
Ashish <ashiishcumar@gmail.com>
2023-05-30 15:18:16 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ const filterRoutes = require("./routes/filterRoutes");
 const bodyParser = require("body-parser");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./dbConfig/mongoDbConnection");
+
 connectDb();
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(errorHandler);
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use("/api/blog", router);
 app.use("/api/admin", adminRoutes);
 app.use("/api/filter", filterRoutes);
