58b447b30e4dab33ab6533a7dccce8bfb00e6229
Ashish <ashiishcumar@gmail.com>
2023-05-30 17:32:42 +0530

    Add files via upload

--- controllers/adminRoutesController.js
+++ controllers/adminRoutesController.js
@@ -6,12 +6,16 @@ const jwt = require("jsonwebtoken");
 const register = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    res.status(400);
+    res.status(400).json({
+      message: "All fields are mandatory!",
+    });
     throw new Error("All fields are mandatory! ");
   }
   const adminAvailable = await admin_data.findOne({ email });
   if (adminAvailable) {
-    res.status(400);
+    res.status(400).json({
+      message: "admin-id already available",
+    });
     throw new Error("admin-id already available");
   }
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -47,25 +51,23 @@ const login = asyncHandler(async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECERT,
-      {expiresIn:"7d"}
+      { expiresIn: "7d" }
     );
     res.status(200).json({ accessToken });
-  }else{
-    res.status(401) 
-    throw new Error('email or password is not valid !')
+  } else {
+    res.status(401).json({ message: "email or password is not valid !" });
+    throw new Error("email or password is not valid !");
   }
   res.json({ message: "user login" });
 });
 
-
-const getAllAdmins = asyncHandler(async(req,res) => {
+const getAllAdmins = asyncHandler(async (req, res) => {
   const allAdmins = await admin_data.find();
-  if(!allAdmins){
+  if (!allAdmins) {
     res.status(400);
     throw new Error("No admin available! ");
   }
   res.status(200).json(allAdmins);
-})
+});
 
-
-module.exports = { register, login, getAllAdmins};
+module.exports = { register, login, getAllAdmins };
